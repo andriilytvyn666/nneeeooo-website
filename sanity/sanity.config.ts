3c5cfef3a0f4fa48ad73db71aad788ba14a67a98
Andrii Lytvyn <lytvyn349@gmail.com>
2024-08-01 22:44:12 +0300

    add sanity media plugin

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,7 +1,9 @@
 import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
+import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import {media} from 'sanity-plugin-media'
+
 export default defineConfig({
   name: 'default',
   title: 'andrii-lytvyn-portfolio',
@@ -10,7 +12,7 @@ export default defineConfig({
   dataset: 'production',
 
   plugins: [
-    deskTool({
+    structureTool({
       structure: (S) =>
         S.list()
           .title('Main')
@@ -19,10 +21,11 @@ export default defineConfig({
               .title('Webdev')
               .child(S.document().title('Webdev').schemaType('webdev').documentId('webdev')),
             ...S.documentTypeListItems().filter(
-              (listItem) => !['webdev', 'webdevItem'].includes(`${listItem.getId()}`)
+              (listItem) => !['webdev', 'webdevItem', 'media.tag'].includes(`${listItem.getId()}`)
             ),
           ]),
     }),
+    media(),
     visionTool(),
   ],
 
