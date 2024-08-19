336b8322b7f81eb587415e82334a0e2bb9af172d
Andrii Lytvyn <lytvyn349@gmail.com>
2024-08-19 06:59:14 +0000

    fix invisible navbar on scroll

--- nuxt/pages/design.vue
+++ nuxt/pages/design.vue
@@ -2,7 +2,7 @@
   <div class="flex">
     <ButtonBack caption="design" />
     <div class="overflow-scroll w-full h-screen">
-      <Navbar caption="design" class="sticky top-0 lg:hidden" />
+      <Navbar caption="design" class="sticky top-0 z-10 lg:hidden" />
       <div
         class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
       >
--- nuxt/pages/photo.vue
+++ nuxt/pages/photo.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex">
     <ButtonBack caption="photo" />
-    <div class="h-screen overflow-scroll">
-      <Navbar caption="photo" class="sticky top-0 lg:hidden" />
+    <div class="overflow-scroll h-screen">
+      <Navbar caption="photo" class="sticky top-0 z-10 lg:hidden" />
       <div
         class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
       >
